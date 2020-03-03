795198c9145e64864f93cff35c38dc6829fcccc9
Your Name <you@example.com>
2020-03-03 17:55:05 +0000

    punto 5 arrreglado mapper, falta organizar valores

--- punto_5/mapper.py
+++ punto_5/mapper.py
@@ -3,4 +3,4 @@ for line in sys.stdin:
         words = line.split(',')
        	array=(words[2].split('-'))
 	if("Date of Transfer"!=array[0]):	
-		print(array[0]+"-"+array[1]+"\t"+str(1))
+		print(array[0]+"\t"+array[1])
--- punto_5/reducer.py
+++ punto_5/reducer.py
@@ -1,31 +1,31 @@
 import sys
 data = sys.stdin.readlines()
+
 for linenum, line in enumerate(data):
         arr=line.replace("\n","").split("\t")
-	aux_arr=arr[0].split("-")
         if linenum == 0:
-	      last_count=1
+              last_count=1
               count=1
-              last_year=aux_arr[0]
-	      last_month=aux_arr[1]
-	      curr_month=aux_arr[1]
+              last_year=arr[0]
+              last_month=arr[1]
+              curr_month=arr[1]
         else:
-              if(last_year==aux_arr[0]):
-		    if(last_month==aux_arr[1]):
-                    	count=count+1
-		    else:
-			if(count>last_count):
-				last_count=count
-				curr_month=last_month
-			count=1
-			last_month=aux_arr[1]
+              if(last_year==arr[0]):
+                    if(last_month==arr[1]):
+                        count=count+1
+                    else:
+                        if(count>last_count):
+                                last_count=count
+                                curr_month=last_month
+                                count=1
+                        last_month=arr[1]
               else:
                     print(last_year+" "+curr_month)
-                    last_year=aux_arr[0]
-		    last_month=aux_arr[1]
+                    last_year=arr[0]
+                    last_month=arr[1]
                     last_count=1
-		    count=1
+                    count=1
 if(count>last_count):
-	last_count=count
-	curr_month=last_month
+        last_count=count
+        curr_month=last_month
 print(last_year+" "+curr_month)
