3dfbdc960aa0096e38028999845ef143f941525a
Carlos <carlosm.cordoba1@correo.usa.edu.co>
2020-03-03 21:28:15 -0500

    p6

--- punto_6/reducer.py
+++ punto_6/reducer.py
@@ -1,17 +1,9 @@
 import sys
 data = sys.stdin.readlines()
-
+arr=set()
 size= len(data)
 for linenum, line in enumerate(data):
         arr=line.replace("\n","").split("\t")
-        if linenum == 0:
-              count=int(arr[1])
-              anterior=arr[0]
-        else:
-              if(anterior==arr[0]):
-                    count=count+int(arr[1])
-              else:
-                    print(str(count)+"\t1")
-                    anterior=arr[0]
-                    count=int(arr[1])
-print(str(count)+"\t1")
+        if arr[0]+"\t"+arr[1] not in arr:
+            arr.add(arr[0]+"\t"+arr[1])
+            print(arr[-1])
