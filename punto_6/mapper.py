4e1416e6e0be2852ec107f7636cb0cd06be2baee
Your Name <you@example.com>
2020-03-04 00:31:00 +0000

    correción mapper punto 6

--- punto_6/mapper.py
+++ punto_6/mapper.py
@@ -2,6 +2,7 @@ import sys
 a=[]
 for line in sys.stdin:
         words = line.split(',')	
-	if("Town/City"!=words[6] and (str(words[6]+"\t"+words[7]) not in a)):	
-		a.append(words[6]+"\t1")
-		print(a[-1])
+	if(len(words)>7):
+		if("Town/City"!=words[6] and (str(words[6]+"\t"+words[7]) not in a)):	
+			a.append(words[6]+"\t1")
+			print(a[-1])
