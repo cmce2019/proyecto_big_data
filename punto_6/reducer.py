a42d66b0837b37899d7849fb92bd48f392096cd0
Carlos <carlosm.cordoba1@correo.usa.edu.co>
2020-03-03 22:40:43 -0500

    2

--- punto_6/reducer.py
+++ punto_6/reducer.py
@@ -4,13 +4,13 @@ size= len(data)
 for linenum, line in enumerate(data):
         arr=line.replace("\n","").split("\t")
         if linenum == 0:
-              count=int(arr[1])
+              count=1
               anterior=arr[0]
         else:
               if(anterior==arr[0]):
-                    count=count+int(arr[1])
+                    count=count+1
               else:
                     print(str(count)+"\t1")
                     anterior=arr[0]
-                    count=int(arr[1])
+                    count=1
 print(str(count)+"\t1")
