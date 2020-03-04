c096d6dd4979e1c40333f5f2f8374f9064cadbc5
Carlos <carlosm.cordoba1@correo.usa.edu.co>
2020-03-03 21:00:12 -0500

    p6

--- punto_6/mapper.py
+++ punto_6/mapper.py
@@ -1,7 +1,6 @@
 import sys
-a=[]
 for line in sys.stdin:
         words = line.split(',')	
 	if(len(words)>7):
 		if("Town/City"!=words[6]):	
-			a.append(words[6]+"\t"+words[7])
+			print(words[6]+"\t"+words[7])
