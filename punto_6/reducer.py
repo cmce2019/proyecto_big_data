cc49a8613aae4a8581a6ee59531c25db1e1bdb42
Carlos <carlosm.cordoba1@correo.usa.edu.co>
2020-03-03 21:54:55 -0500

    2

--- punto_6/reducer.py
+++ punto_6/reducer.py
@@ -5,5 +5,5 @@ size= len(data)
 for linenum, line in enumerate(data):
         arr=line.replace("\n","").split("\t")
         if (str(arr[0]+"\t"+arr[1]) not in arr):
-            arr.append(arr[0]+"\t"+arr[1])
+            arr.append(str(arr[0]+"\t"+arr[1]))
             print(arr[-1])
