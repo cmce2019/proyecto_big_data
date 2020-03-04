ea3e18a66692c0e63e428bbcef76cc318d8bfcda
Carlos <carlosm.cordoba1@correo.usa.edu.co>
2020-03-04 13:45:46 -0500

    punto 6 y 4

--- punto_6/reducer.py
+++ punto_6/reducer.py
@@ -4,11 +4,11 @@ districts = {}
 for linenum, line in enumerate(data):
         arr=line.replace("\n","").split("\t")
         if linenum == 0:
-              districts[arr[0]]=int(arr[1])
+              districts[arr[0]]=1
         else:
               if(arr[0] in districts):
-                    districts[arr[0]]=int(arr[1])+districts[arr[0]]
+                    districts[arr[0]]=1+districts[arr[0]]
               else:
-                    districts[arr[0]]=int(arr[1])
+                    districts[arr[0]]=1
 for key in districts:
       print (str(districts[key])+"\t1")
